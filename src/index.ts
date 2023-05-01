18ffa4e2999346f1f17777d99872bf157600b266
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2023-05-01 15:38:05 -0600

    feat: add missing components to index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
+// Material UI Theme
 export { default as theme } from './theme';
+// Elements.
+export { default as Link } from './components/01-elements/link/link';
+export { default as Logo } from './components/01-elements/logo/logo';
+export { default as SidebarToggle } from './components/01-elements/sidebar-toggle/sidebar-toggle';
+// Components.
+export {
+  default as Brand,
+  type BrandProps,
+} from './components/02-components/brand/brand';
+export {
+  default as BrandingBar,
+  type BrandingBarProps,
+} from './components/02-components/branding-bar/branding-bar';
+export {
+  default as Context,
+  type ContextProps,
+} from './components/02-components/context/context';
+export { default as MainContent } from './components/02-components/main-content/main-content';
 export { default as Placeholder } from './components/02-components/placeholder/placeholder';
+export { default as Sidebar } from './components/02-components/sidebar/sidebar';
+export {
+  default as SidebarMenu,
+  type SidebarMenuProps,
+} from './components/02-components/sidebar-menu/sidebar-menu';
+export { default as TopBar } from './components/02-components/topbar/topbar';
+// Layout.
+export { default as PageLayout } from './components/03-layouts/page-layout/page-layout';
