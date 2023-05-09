6e38b161779986fc3d1fd5db4b23bea72ff62cab
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2023-05-08 19:25:36 -0600

    fix: npm link issues for local development

---
+++ scripts/link-all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# List of dependencies.
+packages="react react-dom @emotion/react @emotion/styled @mui/icons-material @mui/material @remix-run/react"
+
+if [ $# -ne 1 ]; then
+  echo "Please provide the path to the FE app (e.g. '../s4-app')"
+  exit 1
+fi
+
+package_paths=""
+
+# Link dependencies.
+for package in $packages; do
+  package_paths+="${1}/node_modules/${package} "
+done
+
+npm link $package_paths
+
+# Make project linkable
+npm link
