326194743ff31c5ede6d46e9ceaccdc26b5df551
Christopher Martin <ccjjmartin@icloud.com>
2023-09-25 10:45:53 -0500

    fix: make offering list props optional

--- src/components/02-components/offering-list/offering-list.tsx
+++ src/components/02-components/offering-list/offering-list.tsx
@@ -15,18 +15,19 @@ import Pager from '../../01-elements/pager/pager';
 import Breadcrumbs from '../../01-elements/breadcrumbs/breadcrumbs';
 
 type OfferingListProps = {
-  breadcrumb: {
+  title?: string;
+  breadcrumb?: {
     title: string;
     url: string;
   }[];
   children: ReactNode;
   url: string;
-  addUrl: string;
+  addUrl?: string;
   FormElement?: ElementType;
-  totalItems: number;
+  totalItems?: number;
   itemsPerPage: number;
   currentPage: number;
-  categoryFilterValues: {
+  categoryFilterValues?: {
     id: string;
     name: string;
   }[];
@@ -41,6 +42,7 @@ type OfferingListProps = {
 };
 
 export default function OfferingList({
+  title,
   breadcrumb,
   children,
   url,
@@ -56,8 +58,12 @@ export default function OfferingList({
   const theme = useTheme();
 
   const firstItem = itemsPerPage * (currentPage - 1) + 1;
-  const lastItem =
-    totalItems > itemsPerPage ? firstItem + itemsPerPage - 1 : totalItems;
+
+  let lastItem = null;
+  if (totalItems) {
+    lastItem =
+      totalItems > itemsPerPage ? firstItem + itemsPerPage - 1 : totalItems;
+  }
 
   // Stules.
   const containerStyles = {
@@ -101,23 +107,25 @@ export default function OfferingList({
 
   const formInner = (
     <>
-      <FormControl size="small" sx={formItemStyles}>
-        <InputLabel id="category-select-label">Category</InputLabel>
-        <Select
-          labelId="categiry-select-label"
-          id="category-select"
-          label="Category"
-          native={true}
-          name="category"
-          defaultValue={currentFilters.category}
-        >
-          {categoryFilterValues.map((item) => (
-            <option key={item.id} value={item.id}>
-              {item.name}
-            </option>
-          ))}
-        </Select>
-      </FormControl>
+      {categoryFilterValues ? (
+        <FormControl size="small" sx={formItemStyles}>
+          <InputLabel id="category-select-label">Category</InputLabel>
+          <Select
+            labelId="categiry-select-label"
+            id="category-select"
+            label="Category"
+            native={true}
+            name="category"
+            defaultValue={currentFilters.category}
+          >
+            {categoryFilterValues.map((item) => (
+              <option key={item.id} value={item.id}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
+      ) : null}
       {departmentFilterValues ? (
         <FormControl size="small" sx={formItemStyles}>
           <InputLabel id="department-select-label">Department</InputLabel>
@@ -151,40 +159,46 @@ export default function OfferingList({
 
   return (
     <div>
-      <Breadcrumbs items={breadcrumb} />
+      {breadcrumb && (
+        <Breadcrumbs items={breadcrumb} />
+      )}
       <Box sx={titleContainerStyles}>
         <Typography variant="h1" sx={titleStyles}>
-          Offerings
+          {title}
         </Typography>
-        <Button
-          variant="outlined"
-          component={Link}
-          href={addUrl}
-          startIcon={
-            <AddIcon
-              sx={{
-                fontSize: '1rem',
-                mr: theme.spacing(0.5),
-              }}
-            />
-          }
-          sx={{
-            flexShrink: 0,
-            fontWeight: 700,
-          }}
-        >
-          Add offering
-        </Button>
+        {addUrl && (
+          <Button
+            variant="outlined"
+            component={Link}
+            href={addUrl}
+            startIcon={
+              <AddIcon
+                sx={{
+                  fontSize: '1rem',
+                  mr: theme.spacing(0.5),
+                }}
+              />
+            }
+            sx={{
+              flexShrink: 0,
+              fontWeight: 700,
+            }}
+          >
+            Add offering
+          </Button>
+        )}
       </Box>
       <Paper elevation={0} sx={containerStyles}>
-        <Box sx={{ pt: theme.spacing(2), pb: theme.spacing(6) }}>{form}</Box>
-        {totalItems ? (
+        {categoryFilterValues || departmentFilterValues ? (
+          <Box sx={{ pt: theme.spacing(2), pb: theme.spacing(6) }}>{form}</Box>
+        ) : null}
+        {totalItems && lastItem ? (
           <Typography sx={{ mb: theme.spacing(2) }}>
             Showing {firstItem} to {lastItem} of {totalItems} results
           </Typography>
         ) : null}
         <Box sx={listStyles}>{children}</Box>
-        {totalItems > itemsPerPage && (
+        {totalItems && totalItems > itemsPerPage && (
           <Pager
             baseUrl={url}
             count={Math.ceil(totalItems / itemsPerPage)}
