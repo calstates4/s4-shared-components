cf2c1dfd52e6fb0b7175927f86e82c979d452886
Christopher Martin <ccjjmartin@icloud.com>
2023-11-07 11:38:35 -0600

    fix(csu-110): fix bugs

--- src/components/02-components/department-form/department-form.stories.tsx
+++ src/components/02-components/department-form/department-form.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import DepartmentForm from "../department-form/department-form";
-import OrganizationForm from "../organization-form/organization-form";
 
 const meta: Meta<typeof DepartmentForm> = {
   title: 'Components/Department Form',
@@ -8,13 +7,7 @@ const meta: Meta<typeof DepartmentForm> = {
 };
 export default meta;
 
-const description = `<p>
-  We exist to serve the most vulnerable in our communities. We do this by
-  partnering with agencies to give us insight into areas where volunteers can
-  meet the most pressing needs, empowering volunteers to manifest compassion and
-  service as representatives of their communities, and mobilizing churches and
-  businesses to serve in projects in their cities throughout the year!
-</p>`;
+const description = "We exist to serve the most vulnerable in our communities. We do this by partnering with agencies to give us insight into areas where volunteers can meet the most pressing needs, empowering volunteers to manifest compassion and service as representatives of their communities, and mobilizing churches and businesses to serve in projects in their cities throughout the year!";
 
 export const AddDepartment: StoryObj<typeof DepartmentForm> = {
   args: {
@@ -23,7 +16,7 @@ export const AddDepartment: StoryObj<typeof DepartmentForm> = {
   },
 };
 
-export const EditOrganization: StoryObj<typeof OrganizationForm> = {
+export const EditDepartment: StoryObj<typeof DepartmentForm> = {
   args: {
     isEdit: true,
     id: '1',
